2bd39c824f737cf4665a575505edb993856eb47b
Maxwell Babey <babeymax@gmail.com>
2024-02-21 13:53:17 -0500

    Add function to parse the transcript

--- front-end/src/components/Comic.js
+++ front-end/src/components/Comic.js
@@ -6,7 +6,7 @@ import style from '../styles/main.module.css'
 
 const URL_PLAIN = `http://localhost:8080`
 
-function Comic(params) {
+function Comic() {
   const { number } = useParams();
 
   const { maxNum } = useContext(MaxNumContext);
@@ -51,12 +51,19 @@ function Comic(params) {
   return (
     <div>
       <h2 className={style.title}>#{comic.num}: {comic.safe_title}</h2>
+      <div className={style.metadataWrapper}>
+        <p>{getDate(comic.day, comic.month, comic.year)}</p>
+        <p>View count: {comic.view_count}</p>
+      </div>
+      <p className={style.title}></p>
       <div className={style.content}>
         <div className={style.imageWrapper}>
           <img className={style.image} src={comic.img} alt={comic.alt} />
         </div>
         <div className={style.altWrapper}>
           <p className={style.altText}>{comic.alt}</p>
+          <h3 className={style.title}>Transcript</h3>
+          <p className={style.altText}>{parseTranscript(comic.transcript)}</p>
         </div>
         <div className={style.buttonWrapper}>
           {isprev && <button
@@ -84,8 +91,31 @@ function Comic(params) {
   );
 }
 
+function getDate(day, month, year) {
+  return year + "/" + month + "/" + day;
+}
+
 function getRandom(max, min) {
   return Math.ceil(Math.random() * (max - min) + min);
+}
+
+
+
+function parseTranscript(transcript) {
+  const titleMatches = transcript.match(/{{(.*?)}}/g);
+  const contextMatches = transcript.match(/\(\((.*?)\)\)|\[\[(.*?)\]\]/g);
+
+  const regexBraces = /{{(.*?)}}|\[\[(.*?)\]\]|\(\((.*?)\)\)/g;
+  const noMatches = transcript.split(regexBraces).filter(Boolean);
+  console.log(noMatches);
+
+  return (
+    <div>
+      <p>Title: {titleMatches}</p>
+      <p>Context: {contextMatches}</p>
+      <p>Transcription: {noMatches}</p>
+    </div>
+  );
 }
 
 export default Comic;
