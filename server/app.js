2c4a410d62e777b29b1c1af77bd1332d4688073e
Maxwell Babey <babeymax@gmail.com>
2024-02-21 10:21:16 -0500

    Add database connection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const client = require('mongoose');
 const axios = require('axios');
 require('dotenv').config();
 
@@ -7,7 +7,22 @@ const app = express();
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
-app.listen(port, () => {
+async function connectDB() {
+  try {
+    await client.connect(process.env.DB_CONNECTION_STRING);
+  } catch (error) {
+    throw Error('[Error connecting to database]' + error);
+  }
+}
+
+app.listen(port, async () => {
+  try {
+    await connectDB();
+    console.log('Connected to database.');
+  } catch(error) {
+    console.log(error);
+    return;
+  }
   console.log('Server listening on port ' + port);
 })
 
@@ -41,7 +56,7 @@ app.get('/', async (_, res) => {
  */
 app.get('/:number', async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  
+
   const num = req.params.number;
   if (isNaN(num)) {
     res.status(404).send()
@@ -61,3 +76,23 @@ app.get('/:number', async (req, res) => {
 
   res.status(200).send(resContent);
 });
+
+async function incrementDB(num) {
+  try {
+    
+
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    // Increment the value of the field and return the updated document
+    const updatedDocument = await collection.findOneAndUpdate(
+      { /* Your query to find the document */ },
+      { $inc: { [fieldName]: 1 } },
+       { returnOriginal: false }
+    );
+
+    return updatedDocument.value[fieldName];
+  } finally {
+    await client.close();
+  }
+}
